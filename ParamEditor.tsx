df88c45e3ecc2b82f7fda144af71e1a3d616174f
HukakHePak <91820343+HukakHePak@users.noreply.github.com>
2022-07-24 21:32:39 +0400

    feat: new file
    add ts file with parameters editor for react project

---
+++ ParamEditor.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import {ChangeEvent, ReactComponentElement} from "react";
+
+export interface Color {
+    hex: string;
+}
+
+export interface Param {
+    id: number;
+    name: string;
+    type: string;
+}
+
+export interface ParamValue {
+    paramId: number;
+    value: string;
+}
+
+export interface Model {
+    paramValues: ParamValue[];
+    colors: Color[];
+}
+
+interface Props {
+    params: Param[];
+    model: Model;
+    component?: (value: ParamValue, onChange: (e: ChangeEvent<HTMLInputElement>) => void) => React.Component<any, any>;
+}
+
+export class ParamEditor extends React.Component<Props, Model> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = props.model;
+    }
+
+    public getModel(): Model {
+        return this.state;
+    }
+
+    changeHandler(e: ChangeEvent<HTMLInputElement>): void {
+        console.log(this)
+        const params = [...this.state.paramValues];
+        params[0].value = e.target.value;
+
+        this.setState({...this.state, paramValues: params});
+    }
+
+    componentSelector(param: Param): ReactComponentElement<any> {
+        let component;
+
+        switch (param.type) {
+            case 'string':
+                component = <input type="text"
+                                   onChange={(e) => this.changeHandler.call(this, e)}
+                                   value={this.state.paramValues.find((value) =>
+                                       value.paramId === param.id)?.value}
+                />;
+                break;
+
+            default:
+                break;
+        }
+
+
+        return <div key={param.id}>
+            <span>{param.name}</span>
+            {component}
+        </div>;
+    }
+
+    render() {
+        return (
+            <div>
+                <>{this.props.params.map((item) => this.componentSelector.call(this, item))}</>
+            </div>
+        );
+    }
+}
